835283c72374fa05c03872d608ac3ad3fc07e9c1
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2025-12-08 16:13:44 -0800

    fix: increase httpx timeout

--- aind-mgi-service-server/src/aind_mgi_service_server/session.py
+++ aind-mgi-service-server/src/aind_mgi_service_server/session.py
@@ -2,7 +2,7 @@
 
 from typing import AsyncGenerator
 
-from httpx import AsyncClient
+from httpx import AsyncClient, Timeout
 
 from aind_mgi_service_server.configs import Settings
 
@@ -14,5 +14,8 @@ async def get_session() -> AsyncGenerator[AsyncClient, None]:
     Yield an async session object. This will automatically close the session
     when finished.
     """
-    async with AsyncClient(base_url=settings.host.unicode_string()) as session:
+    timeout = Timeout(30.0, read=60.0)
+    async with AsyncClient(
+        base_url=settings.host.unicode_string(), timeout=timeout
+    ) as session:
         yield session
